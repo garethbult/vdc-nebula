ecd3f0b345b9159567daaa209a96c202d0cd2be0
Gareth Bult <gareth@linux.co.uk>
2013-11-28 04:28:38 +0000

    Changes to fix blank images and Marketplace downloads

---
+++ remotes/hooks/vdc/get_image_ids.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import MySQLdb
+import xml.etree.ElementTree as ET
+import os
+import sys
+
+hostname = os.uname()[1]
+
+def xmlParse(elements,xml):
+  """ parse an XML blob into a hierarchical map """
+  for elem in xml:
+    if elem.getchildren() == []:
+      elements[elem.tag] = elem.text
+      continue
+
+    sub = {}
+    xmlParse(sub,elem.getchildren())
+    if elements.has_key(elem.tag):
+      orig = elements[elem.tag]
+
+      if type(orig) <> type([]): orig = [orig]
+      sub=[sub]
+      elements[elem.tag] = orig+sub
+    else:
+      elements[elem.tag] = sub
+
+db = MySQLdb.connect(host="nebula",user="oneadmin",passwd="oneadmin",db="opennebula")
+cur = db.cursor(MySQLdb.cursors.DictCursor)
+cur.execute("FLUSH QUERY CACHE");
+
+output=""
+vmid=int(sys.argv[1])
+cur.execute("SELECT * FROM vm_pool WHERE state <> 6")
+rows = cur.fetchall()
+for row in rows:
+  oid=int(row['oid'])
+  if oid <> vmid: continue
+  elements = {}
+  xmlParse(elements,ET.fromstring(row['body']))
+  temp = elements.get('TEMPLATE',None)
+  if not temp: continue
+  disk = temp.get('DISK',None)
+  if not disk: continue
+  if type(disk) <> type([]): disk = [disk]
+  for d in disk:
+    if d['TM_MAD']    <> 'vdc':    continue
+    output += "ON_"+d['IMAGE_ID']+"_"+str(vmid)
+
+print 'NAME="%s"' % output
--- remotes/hooks/vdc/mkconfig.py
+++ remotes/hooks/vdc/mkconfig.py
@@ -10,6 +10,7 @@ from syslog import syslog,LOG_INFO
 from time import sleep
 
 CONFIG_FILE = "/etc/vdc/config"
+#CONFIG_FILE = "config.temp"
 VDC_NAMES = []
 hostname = os.uname()[1]
 
@@ -29,33 +30,63 @@ def UnlockFile(file):
 def xmlParse(elements,xml):
   """ parse an XML blob into a hierarchical map """
   for elem in xml:
-    if elem.getchildren() <> []:
-      elements[elem.tag] = {}
-      xmlParse(elements[elem.tag],elem.getchildren())
-    else:
+    if elem.getchildren() == []:
       elements[elem.tag] = elem.text
+      continue
+
+    sub = {}
+    xmlParse(sub,elem.getchildren())
+    if elements.has_key(elem.tag):
+      orig = elements[elem.tag]
+
+      if type(orig) <> type([]): orig = [orig]
+      sub=[sub]
+      elements[elem.tag] = orig+sub
+    else:
+      elements[elem.tag] = sub
 
 db = MySQLdb.connect(host="nebula",user="oneadmin",passwd="oneadmin",db="opennebula")
 cur = db.cursor(MySQLdb.cursors.DictCursor) 
 cur.execute("FLUSH QUERY CACHE");
 
-cur.execute("SELECT * FROM datastore_pool");
-for row in cur.fetchall():
+images={}
+cur.execute("SELECT * FROM vm_pool WHERE state <> 6")
+rows = cur.fetchall()
+for row in rows:
   elements = {}
   xmlParse(elements,ET.fromstring(row['body']))
-  if elements['DS_MAD'] == 'vdc': VDC_NAMES.append(elements['NAME'])
+  temp = elements.get('TEMPLATE',None)
+  if not temp: continue
+  disk = temp.get('DISK',None)
+  if not disk: continue
+  if type(disk) <> type([]): disk = [disk]
+  for d in disk:
+    if d['TM_MAD']    <> 'vdc':    continue
+    if d['DATASTORE'] <> hostname: continue
+    vmid = row['oid']
+    imid = d['IMAGE_ID']
+    if not images.has_key(imid):
+      images[imid]=[vmid]
+    else:
+      images[imid]=images[imid]+[vmid]
 
-cur.execute("SELECT * FROM vm_pool WHERE state <> 6");
-pools = cur.fetchall()
+cur.execute("SELECT * FROM datastore_pool WHERE name = '%s'" % hostname)
+rows = cur.fetchall()
+if( len(rows) <> 1 ):
+  print("# ERROR :: needs to be exactly one datastore with name '%s', found %s",hostname,len(rows))
+  sys.exit(1)
 
-dst = open(CONFIG_FILE,"w")
-if not dst:
+oid = int(rows[0]['oid'])
+while True:
+
+  dst = open(CONFIG_FILE,"w")
+  if not dst:
     syslog(LOG_INFO,"* mkconfig ERROR :: config file [%s] is missing" % CONFIG_FILE)
     sys.exit(1)
 
-syslog(LOG_INFO,"* mkconfig :: Waiting on config file")
-while True:
+  syslog(LOG_INFO,"* mkconfig :: Waiting on config file")
   if LockFile(dst): break
+  close(dst)
   time.sleep(1)
 
 syslog(LOG_INFO,"* mkconfig :: Processing ...")
@@ -66,46 +97,40 @@ dst.write("# AUTO-GENERATED - DO NOT EDIT!\n#\n")
 dst.write("[global]\n  host = "+hostname+"\n")
 dst.write("  proto = lsfs\n")
 dst.write("  path = /var/lib/one/images\n")
-dst.write("  size = 10G\n\n")
+dst.write("  size = 10G\n")
 
-for row in pools:
+cur.execute("SELECT * FROM image_pool")
+for row in cur.fetchall():
   elements = {}
   xmlParse(elements,ET.fromstring(row['body']))
-  IMAGE_ID = elements['TEMPLATE']['DISK']['IMAGE_ID']
-  cur.execute("SELECT * FROM image_pool WHERE oid = "+str(IMAGE_ID))
-  for image in cur.fetchall():
-    sub = {}
-    xmlParse(sub,ET.fromstring(image['body']))
-    if sub.has_key('TEMPLATE'):
-      if sub['TEMPLATE'].has_key('CAPACITY'): 
-        store = sub['DATASTORE']
-        if not store in VDC_NAMES: continue
-        if store <> hostname: continue
-        store_id = sub['DATASTORE_ID']
-        persist = int(sub['PERSISTENT'])
-        size = sub['TEMPLATE']['CAPACITY']
-        id = str(elements['ID'])
-        name = "ON_IM_"+id
-        if not persist:
-          cur.execute("SELECT * FROM vdc WHERE name = '"+name+"'")
-          rows = cur.fetchall()
-          if not len(rows):
-            sleep(2)
-            cur.execute("SELECT * FROM vdc WHERE name = '"+name+"'")
-            rows = cur.fetchall()
-            # FIXME :: THIS is a timing issue, needs a better fix
-            if not len(rows): continue
+  if int(elements['DATASTORE_ID']) <> oid: continue
+  persist = int(elements['PERSISTENT'])
 
-          row = rows[0]
-          path = row['path']
-        else:
-          path = sub['SOURCE']
- 
-        dst.write("["+name+"]\n")
-        dst.write("  path = " + path+"\n")
-        dst.write("  size = " + size + "\n")
-        dst.write("  proto = lsfs\n\n")
-	
+  id   = elements.get('ID','0')
+  path = elements.get('SOURCE','none')
+  temp = elements.get('TEMPLATE',{})
+  size = temp.get('CAPACITY','10G')
+  used = elements.get('VMS',None)
+  if(not used): continue
+
+  for vmid in images.get(id,[]):
+    name = "ON_"+str(id)+"_"+str(vmid)
+    dst.write("[%s]\n" % name)
+    dst.write("  size = %s\n" % size)
+    dst.write("  proto = lsfs\n")
+    if not persist:
+      cur.execute("SELECT * FROM vdc WHERE name = '"+name+"'")
+      rows = cur.fetchall()
+      if not len(rows):
+        sleep(2)
+        cur.execute("SELECT * FROM vdc WHERE name = '"+name+"'")
+        rows = cur.fetchall()
+        # FIXME :: THIS is a timing issue, needs a better fix
+        if not len(rows): continue
+      row = rows[0]
+      path = row['path']
+    dst.write("  path = %s\n" % path)
+
 syslog(LOG_INFO,"* mkconfig :: Unlocking ...")
 UnlockFile(dst)
 dst.close()
